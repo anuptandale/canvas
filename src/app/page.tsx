4793e5ff029462c95e2391811ec723a2a5af8cec
anup <anupdineshtandale@gmail.com>
2024-03-13 11:13:07 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,85 @@
-import Image from "next/image";
+"use client"
+import React, { useEffect, useState } from 'react'
+import TextField from '@mui/material/TextField';
+import ImageIcon from '@mui/icons-material/Image';
+import { ColorPicker, useColor } from "react-color-palette";
+import "react-color-palette/css";
 
-export default function Home() {
+const page = () => {
+  const [image, setImage] = useState(null);
+  const [text, setText] = useState("")
+  const [cta, setCta] = useState("")
+  const [color, setColor] = useColor("#561ecb");
+  const [selColor, setSelColor] = useState("red");
+  const handleImageChange = (e: any) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const handletext = (e: any) => {
+    const newText = e.target.value.slice(0, 31); // Limiting to 31 characters
+    setText(newText);
+  }
+  const handlecta = (e: any) => {
+    setCta(e.target.value);
+  }
+  useEffect(()=>{
+    console.log(color);
+    setSelColor(color.hex);
+  },[color])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div style={{ display: "flex", gap: "20px" }}>
+      <div style={{ width: "1080px", height: "1080px",borderRadius:"5px",backgroundColor:"lightgrey", border: "1px solid grey", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <div style={{ height: "400px", width: "500px", border: "3px solid black", background: `repeating-linear-gradient(to bottom, ${selColor},${selColor} 20px, white 20px, white 40px)` }}>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "20px" }}>
+            {image && (
+              <img
+                src={image}
+                alt="Uploaded"
+                style={{ maxWidth: "300px", borderRadius: "20px" }}
+              />
+            )}
+          </div>
+          <div style={{ display: "flex", gap: "50px", width: "400px", alignItems: "center", justifyContent: "center",marginTop:"40px" }}>
+            {image && (<div style={{fontSize:"18px", fontWeight:"600",width:"200px",display:"flex",flexWrap:"wrap"}}>{text}</div>)}
+            {image && (<div style={{fontSize:"18px", border:"2px solid white",backgroundColor:"white",color:"black", borderRadius:"5px"}}>{cta}</div>)}
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div style={{ display: "flex", flexDirection: "column", gap: "30px",marginTop:"30px" }}>
+        <div style={{display:"flex",flexDirection:"column",textAlign:"center"}}>
+        <div style={{fontSize:"19px",fontWeight:"600"}}>Ad Customization</div>
+        <div>Customize your ad and get template according</div>
+        </div>
+        <div style={{ height: "50px", width: "500px", border: "1px solid lightgrey", borderRadius: "5px",display:"flex",alignItems:"center" }}>
+          {/* <label htmlFor="file-input" >
+            Choose a file
+            <input id="file-input" type="file" onChange={handleImageChange} style={{cursor:"pointer"}} />
+          </label> */}
+          <div style={{marginLeft:"10px",display:"flex",alignItems:"center",justifyContent:"center"}}>
+          <ImageIcon sx={{color:"lightblue"}}/>&nbsp; change and add creative image&nbsp;<label htmlFor="files" style={{color:"blue",textDecorationLine:"underline"}}>Select Image</label> <input id="files" type='file' style={{display:"none"}}  onChange={handleImageChange} />
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+        <div style={{opacity:"0.6"}}>-----------------------Edit content--------------------------</div>
+        </div>
+        <div>
+          {/* <input type="text" placeholder='type' onChange={handletext}/> */}
+          <TextField label="Ad Content" variant="outlined" onChange={handletext} sx={{ width: "500px" }} />
+        </div>
+        <div>
+          {/* <input type="text" placeholder='type' onChange={handlecta}/> */}
+          <TextField label="CTA" variant="outlined" onChange={handlecta} sx={{ width: "500px" }} />
+        </div>
+        <ColorPicker color={color} onChange={setColor} />;
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </div>
+  )
 }
+
+export default page;
